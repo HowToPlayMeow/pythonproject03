44777a781e93add57a30fbe33701121071cb96b2
HowToPlayMeow <gamebossmc682@gmail.com>
2023-09-01 12:18:24 +0700

    code v.2 : Meow

--- pytest04.py
+++ pytest04.py
@@ -1,7 +1,7 @@
 W = float(input('ความกว้าง : '))
 H = float(input('ความยาว : '))
 L = float(input('ความสูง : '))
-WHL = float((((W + H) * L) * 2) / 5)
+WHL = float((((W + H) * L) * 2) + ((W * L) * 2) / 5)
 K = f"{WHL:.0f}"
 print('|-----------------------------')
 print(f'| ใช้สีทั้หมด : {K} แกลอน')
